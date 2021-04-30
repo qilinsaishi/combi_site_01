91958c73f4845686452085ad56de0103bdd2e745
justin.chen <cxd032404@hotmail.com>
2021-04-30 10:55:55 +0800

    firstCommit

---
+++ V1/function/release_pre.php
@@ -0,0 +1,12 @@
+<?php
+$command = "git checkout pre && git pull";
+(exec($command, $return));
+echo implode("\n", $return) . "\n";
+unset($return);
+$command = "cp config_pre.php config.php";
+(exec($command, $return));
+echo implode("\n", $return) . "\n";
+unset($return);
+//$command = "php ../sitemap.php";
+//(exec($command,$return));
+//echo implode("\n",$return)."\n";
