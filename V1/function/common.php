eb9df87c80da1953dfdbd63e50efc7cffa14d0fe
justin.chen <cxd032404@hotmail.com>
2021-06-02 12:18:15 +0800

    merge

--- V1/function/common.php
+++ V1/function/common.php
@@ -165,7 +165,7 @@
         {
             if(trim($file)!="")
             {
-                echo '<link href="'.$config['site_url'].'/js/'.$file.'.js" type="text/javascript" />';
+                echo '<script src="'.$config['site_url'].'/js/'.$file.'.js" type="text/javascript" />';
             }
         }
         echo '<script src="'.$config['site_url'].'/js/bootstrap.js" type="text/javascript"></script>';
